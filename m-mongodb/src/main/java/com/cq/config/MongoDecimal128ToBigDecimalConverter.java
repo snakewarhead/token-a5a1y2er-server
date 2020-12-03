00855bb868d4be7168c6be8a772e37bff887f54e
linsilence <linlinsilence@gmail.com>
2020-12-03 11:14:58 +0800

    change name 2

---
+++ m-mongodb/src/main/java/com/cq/config/MongoDecimal128ToBigDecimalConverter.java
@@ -0,0 +1,19 @@
+package com.cq.config;
+
+import org.bson.types.Decimal128;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by lin on 2020-09-28.
+ */
+@ReadingConverter
+public class MongoDecimal128ToBigDecimalConverter implements Converter<Decimal128, BigDecimal> {
+
+    @Override
+    public BigDecimal convert(Decimal128 source) {
+        return source.bigDecimalValue();
+    }
+}
