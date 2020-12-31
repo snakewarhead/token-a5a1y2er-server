d06f590e3033446760ce765cb90ad711620c75f4
linsilence <linlinsilence@gmail.com>
2020-12-31 16:21:29 +0800

    ExchangeTakerLongShortRatio, 2

--- s-exchange-grabber-binance/src/main/java/com/cq/exchange/task/TakerLongShortRatioGrabber.java
+++ s-exchange-grabber-binance/src/main/java/com/cq/exchange/task/TakerLongShortRatioGrabber.java
@@ -5,10 +5,12 @@ import com.cq.exchange.entity.ExchangeTakerLongShortRatio;
 import com.cq.exchange.enums.ExchangeEnum;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.knowm.xchange.binance.BinanceAdapters;
 import org.knowm.xchange.binance.BinanceFuturesExchange;
 import org.knowm.xchange.binance.dto.marketdata.BinanceTakerLongShortRatio;
 import org.knowm.xchange.binance.dto.marketdata.KlineInterval;
 import org.knowm.xchange.binance.service.BinanceFuturesMarketDataServiceRaw;
+import org.knowm.xchange.currency.CurrencyPair;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import java.util.List;
@@ -29,19 +31,31 @@ public class TakerLongShortRatioGrabber implements Runnable {
 
     @Override
     public void run() {
-        BinanceFuturesExchange exchange = (BinanceFuturesExchange) exchangeContext.getStreamingExchangeCurrent();
-        BinanceFuturesMarketDataServiceRaw service = (BinanceFuturesMarketDataServiceRaw) exchange.getMarketDataService();
 
         symbols.forEach(s -> {
-            try {
-                List<BinanceTakerLongShortRatio> ls = service.takerlongshortRatio(s, period, 100, null, null);
-                List<ExchangeTakerLongShortRatio> lsAdapt = ls.stream().map(this::adapt).collect(Collectors.toList());
+            new GrabberOne(s).run();
+        });
+    }
 
-                a ThreadPoolTaskScheduler wrapper can maintain a pool in which store taskes that need run.
+    @RequiredArgsConstructor
+    class GrabberOne implements Runnable {
+
+        private final String symbol;
+
+        @Override
+        public void run() {
+            try {
+                BinanceFuturesExchange exchange = (BinanceFuturesExchange) exchangeContext.getStreamingExchangeCurrent();
+                BinanceFuturesMarketDataServiceRaw service = (BinanceFuturesMarketDataServiceRaw) exchange.getMarketDataService();
+
+                List<BinanceTakerLongShortRatio> ls = service.takerlongshortRatio(symbol, period, 100, null, null);
+                List<ExchangeTakerLongShortRatio> lsAdapt = ls.stream().map(e -> adapt(symbol, e)).collect(Collectors.toList());
+
+                a ThreadPoolTaskScheduler wrapper can maintain a pool in which store taskes that need run with times limit.
             } catch (Exception e) {
                 log.error(e.getMessage(), e);
             }
-        });
+        }
     }
 
     public String cron(String periodStr) {
@@ -49,7 +63,7 @@ public class TakerLongShortRatioGrabber implements Runnable {
         this.period = KlineInterval.getEnum(periodStr);
 
         // "5m","15m","30m","1h","2h","4h","6h","12h","1d"
-        // need fast 5 times
+        // need fast 5 multiple
         // TODO: If No new data that grabbing in current period, that need grabbing again for a while.
         if ("5m".equals(periodStr)) {
             return "3 */1 * * * ?";
@@ -60,10 +74,18 @@ public class TakerLongShortRatioGrabber implements Runnable {
         throw new RuntimeException("This period is not supported. " + periodStr);
     }
 
-    public ExchangeTakerLongShortRatio adapt(BinanceTakerLongShortRatio r) {
+    public ExchangeTakerLongShortRatio adapt(String symbol, BinanceTakerLongShortRatio r) {
+        CurrencyPair pair = BinanceAdapters.adaptSymbol(symbol);
         return ExchangeTakerLongShortRatio.builder()
                 .exchangeId(ExchangeEnum.BINANCE.getCode())
-                slfj
+                .tradeType(exchangeContext.getTradeType().getCode())
+                .symbol(symbol)
+                .pair(pair.toString())
+                .baseSymbol(pair.base.getCurrencyCode())
+                .buyVol(r.getBuyVol())
+                .sellVol(r.getSellVol())
+                .buySellRatio(r.getBuySellRatio())
+                .time(r.getTimestamp())
                 .build();
     }
 }
