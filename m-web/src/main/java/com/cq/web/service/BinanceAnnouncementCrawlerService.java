e2f91dd580dca9b6f0e719d7ddafbcf7133f1fd7
linsilence <linlinsilence@gmail.com>
2021-08-13 11:52:31 +0800

    web, binance_announcement_crawl 2

--- m-web/src/main/java/com/cq/web/service/BinanceAnnouncementCrawlerService.java
+++ m-web/src/main/java/com/cq/web/service/BinanceAnnouncementCrawlerService.java
@@ -3,6 +3,7 @@ package com.cq.web.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Spider;
 
 import java.util.List;
@@ -20,11 +21,14 @@ public class BinanceAnnouncementCrawlerService {
     private Spider spider;
 
     public void init(List<String> notices) {
-        spider = new Spider(new BinanceAnnouncementPageProcess()).addPipeline(new BinanceAnnouncementPipeline(serviceContext, notices)).thread(1);
+        spider = new Spider(new BinanceAnnouncementPageProcess())
+                .addPipeline(new BinanceAnnouncementPipeline(serviceContext, notices))
+                .thread(1)
+                .setExitWhenComplete(false);
         spider.start();
     }
 
     public void loop() {
-        spider.addUrl(BinanceAnnouncementPageProcess.HOST);
+        spider.addRequest(new Request(BinanceAnnouncementPageProcess.HOST).putExtra(Request.CYCLE_TRIED_TIMES, -1));
     }
 }
